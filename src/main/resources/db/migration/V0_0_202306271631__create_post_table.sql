2fce6cda54a6b626b60cf368b68a626af0317e43
michal.skrha <michal.skrha@kodeosolutions.sk>
2023-06-27 17:13:02 +0200

    General plugins with database migration.

---
+++ src/main/resources/db/migration/V0_0_202306271631__create_post_table.sql
@@ -0,0 +1,16 @@
+create table if not exists post
+(
+    id                 bigserial not null
+        constraint post_pkey
+            primary key,
+
+    created_by         varchar(255),
+    created_date       timestamp,
+    last_modified_by   varchar(255),
+    last_modified_date timestamp,
+    title              varchar(255),
+    body               text,
+    user_id            bigint not null
+        constraint fk859n2jvi2763ret7263et24e6
+            references users
+);
